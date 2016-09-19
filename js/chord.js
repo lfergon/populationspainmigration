b00f20842e4b045b94ee9c15fa24041eb5b1de8a
Luis <lfergon@gmail.com>
2016-09-19 20:45:06 +0200

    Clean css structure

--- js/chord.js
+++ js/chord.js
@@ -28,61 +28,61 @@ svg.append("circle")
 d3.csv("regions.csv", function(regions) {
   d3.json("matrix.json", function(matrix) {
 
-  // Compute the chord layout.
-  layout.matrix(matrix);
-  
-  // Add a group per neighborhood.
-  var group = svg.selectAll(".group")
-    .data(layout.groups)
-    .enter().append("g")
-    .attr("class", "group")
-    .on("mouseover", mouseover);
+    // Compute the chord layout.
+    layout.matrix(matrix);
+    
+    // Add a group per neighborhood.
+    var group = svg.selectAll(".group")
+      .data(layout.groups)
+      .enter().append("g")
+      .attr("class", "group")
+      .on("mouseover", mouseover);
 
-  // Add a mouseover title.
-  group.append("title").text(function(d, i) {
-    return regions[i].name + ": " + d.value + " of origins";
-  });
-  
-  // Add the group arc.
-  var groupPath = group.append("path")
-    .attr("id", function(d, i) { 
-      return "group" + i; 
-    })
-    .attr("d", arc)
-    .style("fill", function(d, i) { 
-      return regions[i].color; 
+    // Add a mouseover title.
+    group.append("title").text(function(d, i) {
+      return regions[i].name + ": " + d.value + " of origins";
     });
-  
-  // Add a text label.
-  var groupText = group.append("text")
-    .attr("x", 6)
-    .attr("dy", 15);
-  
-  groupText.append("textPath")
-    .attr("xlink:href", function(d, i) { 
-      return "#group" + i; 
+    
+    // Add the group arc.
+    var groupPath = group.append("path")
+      .attr("id", function(d, i) { 
+        return "group" + i; 
+      })
+      .attr("d", arc)
+      .style("fill", function(d, i) { 
+        return regions[i].color; 
+      });
+    
+    // Add a text label.
+    var groupText = group.append("text")
+      .attr("x", 6)
+      .attr("dy", 15);
+    
+    groupText.append("textPath")
+      .attr("xlink:href", function(d, i) { 
+        return "#group" + i; 
+      })
+      .text(function(d, i) { 
+        return " " + regions[i].name; 
+      });
+    
+    // Remove the labels that don't fit. :(
+    groupText.filter(function(d, i) { 
+      return groupPath[0][i].getTotalLength() / 2 - 16 < this.getComputedTextLength(); 
     })
-    .text(function(d, i) { 
-      return " " + regions[i].name; 
-    });
-  
-  // Remove the labels that don't fit. :(
-  groupText.filter(function(d, i) { 
-    return groupPath[0][i].getTotalLength() / 2 - 16 < this.getComputedTextLength(); 
-  })
-  .remove();
-  
-  // Add the chords.
-  var chord = svg.selectAll(".chord")
-    .data(layout.chords)
-    .enter().append("path")
-    .attr("class", "chord")
-    .style("fill", function(d) { 
-      return regions[d.source.index].color; 
-    })
-    .attr("d", path);
-  
-    // Add an elaborate mouseover title for each chord.
+    .remove();
+    
+    // Add the chords.
+    var chord = svg.selectAll(".chord")
+      .data(layout.chords)
+      .enter().append("path")
+      .attr("class", "chord")
+      .style("fill", function(d) { 
+        return regions[d.source.index].color; 
+      })
+      .attr("d", path);
+    
+      // Add an elaborate mouseover title for each chord.
     chord.append("title").text(function(d) {
       return regions[d.source.index].name
         + " → " + regions[d.target.index].name
@@ -91,7 +91,7 @@ d3.csv("regions.csv", function(regions) {
         + " → " + regions[d.source.index].name
         + ": " + d.target.value;
     });
-    
+      
     function mouseover(d, i) {
       chord.classed("fade", function(p) {
         return p.source.index != i && p.target.index != i;
